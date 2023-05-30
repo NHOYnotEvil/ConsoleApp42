5d23ec76deb93f659aed35d0c5b734d05b1297da
NHOY <NHOY@2210DR000919>
2023-05-31 01:08:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stepInExample = 7;
+            int startNumberExample = 5;
+            int resultExample = 96;
+
+            for (int i = startNumberExample; i <= resultExample; i += stepInExample)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
